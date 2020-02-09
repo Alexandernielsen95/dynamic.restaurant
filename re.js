3338e069d2a2a78b8e671c0a1b2f426f5e1cffab
alexandernielsen95 <alexandernielsen251gmail.com>
2020-02-09 10:23:07 +0100

    js

--- re.js
+++ re.js
@@ -3,4 +3,24 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
 return response.json()
 })
 
-.then()
+.then(function (data) {
+    showData(data)
+})
+
+function showData (jsonData) {
+    console.log(jsonData)
+}
+
+jsonDate.forEach(showAsingleDish)
+
+function showAsingleDish (dish)
+cosole.log(dish)
+
+const template = document.querySelector("#dishTemplate").content;
+
+const copy = template.cloneNode("true");
+
+copy.querySelector("#head").textContent("hi mom");
+
+document.querySelector("#starters").appendChild(copy);
+
