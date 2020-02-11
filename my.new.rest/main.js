d98956ba39b1214fb9d691c244df439822fabf4f
alexandernielsen95 <alexandernielsen251gmail.com>
2020-02-11 07:35:15 +0100

    my rest

--- my.new.rest/main.js
+++ my.new.rest/main.js
@@ -20,23 +20,26 @@ function showSingleDish(dish) {
     const copy = template.cloneNode("true");
 
     copy.querySelector("h3").textContent = dish.name;
-    copy.querySelector("p").textContent = dish.shortdescription;
+    copy.querySelector(".info").textContent = dish.shortdescription;
+    copy.querySelector(".soldout").textContent = dish.soldout;
 
 
-     if(dish.discount){
+    if (dish.discount) {
+
         copy.querySelector(".price-discount span").textContent = dish.price;
 
         const newprice = Math.round(dish.price - dish.price * dish.discount / 100);
 
-         copy.querySelector(".price-full span").textContent = newprice;
-    }
-    else {
-       copy.querySelector(".price-discount").remove()
+        copy.querySelector(".price-full span").textContent = newprice;
+    } else {
+        copy.querySelector(".price-discount").remove()
         copy.querySelector(".price-full").textContent = dish.price;
     }
 
+    const base = "https://kea-alt-del.dk/t5/site/imgs/";
+    const smallImg = base + "small/" + dish.image + "-sm.jpg";
+    copy.querySelector("img").setAttribute("src", smallImg)
 
     document.querySelector("#starters").appendChild(copy)
 
 }
-
